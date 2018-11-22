e709d3823bf9c0234dd68191ca70a7a0f1435005
edquijano89 <43075042+edquijano89@users.noreply.github.com>
2018-11-22 17:34:11 -0600

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-wget -O splunk-7.2.1-be11b2c46e23-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.1&product=splunk&filename=splunk-7.2.1-be11b2c46e23-Linux-x86_64.tgz&wget=true'
+wget -O splunk-7.2.1-be11b2c46e23-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.1&product=splunk&filename=splunk-7.2.1-be11b2c46e23-linux-2.6-amd64.deb&wget=true'
