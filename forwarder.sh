6d8722ff817be8f261bbb5bc75279838878e154b
edquijano89 <43075042+edquijano89@users.noreply.github.com>
2018-11-22 18:29:37 -0600

    Update forwarder.sh

--- forwarder.sh
+++ forwarder.sh
@@ -1,3 +1,3 @@
-
+#!/bin/bash
 
 wget -O splunkforwarder-7.1.2-a0c72a66db66-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.2&product=universalforwarder&filename=splunkforwarder-7.1.2-a0c72a66db66-Linux-x86_64.tgz&wget=true'
