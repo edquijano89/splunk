fa15ba6eca29fbe8b9f94aeb0c4ea635568fdce1
edquijano89 <43075042+edquijano89@users.noreply.github.com>
2018-11-22 18:29:14 -0600

    Create forwarder.sh

---
+++ forwarder.sh
@@ -0,0 +1,3 @@
+
+
+wget -O splunkforwarder-7.1.2-a0c72a66db66-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.2&product=universalforwarder&filename=splunkforwarder-7.1.2-a0c72a66db66-Linux-x86_64.tgz&wget=true'
